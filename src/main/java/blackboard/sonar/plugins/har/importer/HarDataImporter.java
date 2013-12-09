38ece583e3c54da7b443194c0d17dc60c16bcfd7
Joey <tu.yijun@blackboard.com.cn>
2013-12-10 01:59:33 +0800

    Revert "Submit the framework code"
    This reverts commit bf3ffdc36b847fea93fb2e12275906425c8c426e.

--- src/main/java/blackboard/sonar/plugins/har/importer/HarDataImporter.java
+++
@@ -1,9 +0,0 @@
-package blackboard.sonar.plugins.har.importer;
-
-import blackboard.sonar.plugins.har.exception.HarPluginException;
-
-public interface HarDataImporter {
-	
-	public void importData() throws HarPluginException;
-
-}
